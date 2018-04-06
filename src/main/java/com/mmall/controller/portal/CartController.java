6868f2ad950a0c4a2fb8334788dc5cb89b149f0b
ciker_killer <2050459191@qq.com>
2018-04-06 12:16:25 +0800

    new add

---
+++ src/main/java/com/mmall/controller/portal/CartController.java
@@ -0,0 +1,123 @@
+package com.mmall.controller.portal;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mmall.common.MmallConstant;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.ICartService;
+import com.mmall.vo.CartVo;
+
+@Controller
+@RequestMapping("/cart/")
+public class CartController {
+
+    @Autowired
+    private ICartService iCartService;
+
+
+
+    @RequestMapping(value="list.do",method=RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<CartVo> list(HttpSession session){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.list(user.getId());
+    }
+
+    @RequestMapping(value="add.do",method=RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<CartVo> add(HttpSession session, Integer count, Integer productId){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.add(user.getId(),productId,count);
+    }
+
+
+
+    @RequestMapping(value="update.do",method=RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<CartVo> update(HttpSession session, Integer count, Integer productId){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.update(user.getId(),productId,count);
+    }
+
+    @RequestMapping(value="delete_product.do",method=RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<CartVo> deleteProduct(HttpSession session,String productIds){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.deleteProduct(user.getId(),productIds);
+    }
+
+
+    @RequestMapping(value="select_all.do",method=RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<CartVo> selectAll(HttpSession session){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.selectOrUnSelect(user.getId(),null,MmallConstant.Cart.CHECKED);
+    }
+
+    @RequestMapping(value="un_select_all.do",method=RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<CartVo> unSelectAll(HttpSession session){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.selectOrUnSelect(user.getId(),null,MmallConstant.Cart.UN_CHECKED);
+    }
+
+
+
+    @RequestMapping(value="select.do",method=RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<CartVo> select(HttpSession session,Integer productId){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.selectOrUnSelect(user.getId(),productId,MmallConstant.Cart.CHECKED);
+    }
+
+    @RequestMapping(value="un_select.do",method=RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<CartVo> unSelect(HttpSession session,Integer productId){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iCartService.selectOrUnSelect(user.getId(),productId,MmallConstant.Cart.UN_CHECKED);
+    }
+
+
+
+    @RequestMapping(value="get_cart_product_count.do",method=RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<Integer> getCartProductCount(HttpSession session){
+        User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createBySuccess(0);
+        }
+        return iCartService.getCartProductCount(user.getId());
+    }
+}
