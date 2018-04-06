17a44c8a600fee92f43a958a11449bed9f4f1d4b
ciker_killer <2050459191@qq.com>
2018-04-06 12:22:34 +0800

    update

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -35,7 +36,7 @@ public class OrderController {
     private IOrderService iOrderService;
 
 
-    @RequestMapping("create.do")
+    @RequestMapping(value="create.do",method=RequestMethod.GET)
     @ResponseBody
     public ServerResponse create(HttpSession session, Integer shippingId){
         User user = (User)session.getAttribute(MmallConstant.CURRENT_USER);
